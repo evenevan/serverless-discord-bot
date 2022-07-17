d17506957c36c682809c1583bf01635b4720727a
Attituding <84680760+Attituding@users.noreply.github.com>
2022-07-17 11:40:12 -0700

    feat: guildOnly precondition

---
+++ src/preconditions/guildOnly.ts
@@ -0,0 +1,50 @@
+import { isGuildInteraction } from 'discord-api-types/utils/v10';
+import {
+    type APIChatInputApplicationCommandInteraction,
+    InteractionResponseType,
+    MessageFlags,
+    type APIBaseInteraction,
+    InteractionType,
+} from 'discord-api-types/v10';
+import { type ENV } from '../@types/env';
+import { APIResponse } from '../structures/APIResponse';
+import { type Command } from '../structures/Command';
+import { Precondition } from '../structures/Precondition';
+
+export class GuildOnlyPrecondition extends Precondition {
+    public constructor(env: ENV) {
+        super({
+            env: env,
+            name: 'guildOnly',
+        });
+    }
+
+    public async chatInput(command: Command, interaction: APIChatInputApplicationCommandInteraction) {
+        if (isGuildInteraction(interaction)) {
+            return undefined;
+        }
+
+        console.warn(
+            `${this.constructor.name}:`,
+            'User failed guildOnly precondition.',
+            `Command: ${command.structure.name}.`,
+            `User: ${interaction.member?.user.id ?? interaction.user?.id}.`,
+        );
+
+        return this.error(interaction);
+    }
+
+    public async error(interaction: APIBaseInteraction<InteractionType, unknown>) {
+        const { i18n } = interaction;
+
+        return new APIResponse({
+            type: InteractionResponseType.ChannelMessageWithSource,
+            data: {
+                content: i18n.getMessage(
+                    'preconditionGuildOnly',
+                ),
+                flags: MessageFlags.Ephemeral,
+            },
+        });
+    }
+}
